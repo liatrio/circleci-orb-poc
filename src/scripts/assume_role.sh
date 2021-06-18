f24d9e979da4c5cb9c7cce5795a291fc3b8b6b43
Samantha Mathistad <ssmathistad@ip-10-0-0-193.ec2.internal>
2021-06-18 07:46:05 -0700

    Test script usage [semver:patch]

--- src/scripts/assume_role.sh
+++
@@ -1,7 +0,0 @@
-assumed_role=$(aws sts assume-role --role-arn $<<parameters.role-arn>> --role-session-name <<parameters.role-session-name>>)
-aws_access_key_id=$(echo $assumed_role | jq .Credentials.AccessKeyId | xargs)
-aws_secret_access_key=$(echo $assumed_role | jq .Credentials.SecretAccessKey | xargs)
-aws_session_token=$(echo $assumed_role | jq .Credentials.SessionToken | xargs)
-echo "export AWS_ACCESS_KEY_ID=$aws_access_key_id" >> $BASH_ENV
-echo "export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key" >> $BASH_ENV
-echo "export AWS_SESSION_TOKEN=$aws_session_token" >> $BASH_ENV
