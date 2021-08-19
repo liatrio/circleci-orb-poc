7bb2fd489b66ec3968b4df5fb4c3da761cd47280
JGuzman4 <jeffg@liatrio.com>
2021-08-19 10:55:00 -0700

    trying to put an item into a table

--- src/scripts/metrics.sh
+++ src/scripts/metrics.sh
@@ -1,4 +1,9 @@
-aws dynamodb list-tables
 curl --request GET \
      --url https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job \
-     --header "authorization: Basic $API_TOKEN"
+     --header "authorization: Basic $API_TOKEN" > jobs.json
+
+cat jobs.json | jq -c '.items[] | select(.name | contains("deploy"))' > deploy-result.json
+
+aws dynamodb put-item \
+    --table-name deployments \
+    --item file://deploy-result.json
